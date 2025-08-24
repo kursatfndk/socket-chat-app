4a0b928e4776148b792a7e663da26d69befa0235
Kürşat Emre Fındık <147005854+kursatfndk@users.noreply.github.com>
2025-08-24 13:10:05 +0300

    Create chat_server.py

---
+++ chat_server.py
@@ -0,0 +1,31 @@
+import socket
+
+PORT = 12345
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDRESS = (SERVER,PORT)
+FORMAT = "utf-8"
+BYTESIZE = 1024
+DİSCONNECT_MESSAGE = "quit"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDRESS)
+
+server.listen()
+print("server çalışıyor...\n")
+
+client_socket, client_address = server.accept()
+client_socket.send("server bağlantınız yapıldı.\n".encode(FORMAT))
+
+while True:
+    message = client_socket.recv(BYTESIZE).decode(FORMAT)
+
+    if message == DİSCONNECT_MESSAGE:
+        client_socket.send("quit".encode(FORMAT))
+        print("çıkış yapıldı...")
+        break
+    else:
+        print(f"{message}\n")
+        message = input("mesaj: ")
+        client_socket.send(message.encode(FORMAT))
+
+client_socket.close()
